5f18f270b11266ef56699ceb71f82cf1566ce7f5
Cristian Cortés <cristian.cortes@brm.com.co>
2016-09-14 11:04:14 -0500

    update de las tareas de gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,23 @@
-var gulp        = require('gulp'),
-	browserSync = require('browser-sync').create(),
-	concat      = require('gulp-concat'), 
-	rename      = require('gulp-rename'),
-	minifyCSS   = require('gulp-minify-css'),
-	uglify      = require('gulp-uglify'),
-	data        = require('gulp-data'),
-	header      = require('gulp-header'),
-	sourcemaps  = require('gulp-sourcemaps'),
-	stylus      = require('gulp-stylus'),
-	nib         = require('nib'),
-	pug         = require('gulp-pug');
+var gulp      = require('gulp'),
+  browserSync = require('browser-sync').create(),
+  concat      = require('gulp-concat'), 
+  rename      = require('gulp-rename'),
+  minifyCSS   = require('gulp-minify-css'),
+  uglify      = require('gulp-uglify'),
+  data        = require('gulp-data'),
+  header      = require('gulp-header'),
+  sourcemaps  = require('gulp-sourcemaps'),
+  stylus      = require('gulp-stylus'),
+  nib         = require('nib'),
+  fs          = require('fs'),
+  cache       = require('gulp-cache');,
+  pug         = require('gulp-pug');
 
-var debug = true;
 //banner
-var pkg = require('./frontend.json');
+var pkg = require('./pink-black.json');
+
+var debug = false;
+
 var banner = ['/**',
   ' * <%= pkg.name %> - <%= pkg.description %>',
   ' * @version v<%= pkg.version %>',
@@ -26,9 +30,8 @@ var banner = ['/**',
 ].join('\n');
 
 //arreglo concatenar JS en el orden en el que se cargan
-var jsLibs = ['publication/js/libs/underscore-min.js',
-              'publication/js/libs/angular.min.js', 
-              'publication/js/libs/angular-route.min.js'];
+var jsLibs = ['publication/js/libs/jquery.js',
+              'publication/js/libs/bootstrap.min.js'];
 
 
 //Tarea para comprimir las libreriras JS
@@ -46,37 +49,51 @@ gulp.task('libs', function() {
 //tarea para compilar stylus
 gulp.task('css', function () {
   return gulp.src('src/stylus/main.styl')
-  	.pipe(header(banner, { pkg : pkg } ))
-  	.pipe(sourcemaps.init()) //cargamos tarea de sourcemaps
-	.pipe(stylus({ //iniciamos stylus
-		use: nib(), // cargamos nib para uso de css3
-		compress: false
-	})) 
-	.pipe(rename('style.css')) //renombramos el archivo
-	.pipe(gulp.dest('./publication/css')) // destino del archivo
-	.pipe(sourcemaps.write('.')) //creamos sourcemap aparte
-	.pipe(gulp.dest('./publication/css'))
-	.pipe(browserSync.reload({
+    .pipe(header(banner, { pkg : pkg } ))
+    .pipe(sourcemaps.init()) //cargamos tarea de sourcemaps
+  .pipe(stylus({ //iniciamos stylus
+    use: nib(), // cargamos nib para uso de css3
+    compress: false
+  })) 
+  .pipe(rename('style.css')) //renombramos el archivo
+  .pipe(gulp.dest('./publication/css')) // destino del archivo
+  .pipe(sourcemaps.write('.')) //creamos sourcemap aparte
+  .pipe(gulp.dest('./publication/css'))
+  .pipe(browserSync.reload({
       stream: true
     }))
 
 });
 
+//Concatenar y minificar CSS
+gulp.task('minicss', function() {
+  return gulp.src(['publication/css/**/*.css', '!publication/css/**/'+pkg.name+'.min.css'])
+  .pipe(minifyCSS())
+  .pipe(concat(pkg.name +'.min.css'))
+  .pipe(gulp.dest('publication/css'))
+
+});
+
+
+//Render de pug
 gulp.task('views', function() {
   return gulp.src('src/views/*.pug')
-  	.pipe(data( function (file) {
-  		return {
-  			debug: debug
-  		};
-  	}))
-	.pipe(pug({
-		pretty: true
-		}))
-	.pipe(gulp.dest('./publication'))
-	.pipe(browserSync.reload({
+    .pipe(data( function (file) {
+      return {
+        debug: debug,
+        name: pkg.name
+      };
+    }))
+  .pipe(pug({
+    pretty: true
+    }))
+  .pipe(gulp.dest('./publication'))
+  .pipe(browserSync.reload({
       stream: true
     }))
 });
+
+
 
 //Tarea base de browsersync para crear el servidor
 gulp.task('browserSync', function() {
@@ -87,12 +104,18 @@ gulp.task('browserSync', function() {
   })
 });
 
+gulp.task('limpiar', function (done) {
+  return cache.clearAll(done);
+});
+
 
 //tarea que observa cambios para recargar el navegador
 gulp.task('watch', ['browserSync', 'views', 'css'], function (){
 
-	gulp.watch('src/views/*.pug', ['views']);
-	gulp.watch('src/stylus/**/*.styl', ['css']);
-	gulp.watch('publication/js/**/*.js', browserSync.reload); 
+  gulp.watch('src/stylus/**/*.styl',  ['css']);
+  gulp.watch(['src/views/*.pug', 'src/templates/**/*.pug'],  ['views']);
+  gulp.watch('publication/js/**/*.js', browserSync.reload);
+  gulp.watch('publication/images/**/*.{gif,svg,jpg,png}', browserSync.reload);
+  gulp.watch('publication/fonts/**/*.{svg,eot,ttf,woff,woff2}', browserSync.reload);
 
 });
