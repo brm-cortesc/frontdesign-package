7c65e6cbe539b767ea2563f9d22b92c3df8a0047
Cristian Cortés <cristian.cortes@brm.com.co>
2016-12-20 10:48:25 -0500

    added some ES6 syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp      = require('gulp'),
+const gulp      = require('gulp'),
   browserSync = require('browser-sync').create(),
   concat      = require('gulp-concat'), 
   rename      = require('gulp-rename'),
@@ -9,20 +9,56 @@ var gulp      = require('gulp'),
   sourcemaps  = require('gulp-sourcemaps'),
   stylus      = require('gulp-stylus'),
   nib         = require('nib'),
-  fs          = require('fs'),
+  path        = require('path'),
   cache       = require('gulp-cache'),
+  coffee      = require('gulp-coffee'),
   pug         = require('gulp-pug'),
+  uncss       = require('gulp-uncss'),
   argv        = require('yargs').argv;  
 
 //data
-var pkg   = require('./frontend.json'),
-    debug = argv.debug;
+const pkg   = require('./frontend.json'),
+      debug = argv.debug;
 
+
+//Rutas
+const routes = {
+  app: path.join(__dirname, 'publication/'),
+  src: path.join(__dirname, 'src/'),
+  css: 'css/',
+  stylus: 'stylus/',
+  views: 'views/',
+  templates: 'templates/',
+  js: 'js/'
+};
+
+/**Routes
+
+  routes.app + routes.js = 'publication/js/'
+  routes.app + routes.css = 'publication/css/'
+  routes.src + routes.stylus = 'src/stylus/'
+  routes.src + routes.views = 'src/views/'
+  routes.src + routes.templates = 'src/templates/'
+
+**/
+
+gulp.task('log',() =>{
+
+  app = path.join(__dirname, 'publication/')
+
+  console.log(app);
+
+});
 
 //Error handler//
-function onError(err) { console.log(err); this.emit('end'); };
+function onError(err) {
+  console.log(err);
+  this.emit('end');
 
-var banner = ['/**',
+};
+
+
+const banner = ['/**',
   ' * <%= pkg.name %> - <%= pkg.description %>',
   ' * @version v<%= pkg.version %>',
   ' * @link <%= pkg.homepage %>',
@@ -34,29 +70,29 @@ var banner = ['/**',
 ].join('\n');
 
 //arreglo concatenar JS en el orden en el que se cargan
-var jsLibs = ['publication/js/libs/jquery.js',
-              'publication/js/libs/jquery.validate.js',
-              'publication/js/libs/jquery.bxslider.min.js',
-              'publication/js/libs/bootstrap.min.js',
-              'publication/js/libs/velocity.min.js'
-              ];
+const jsLibs = [
+  routes.app + routes.js +'libs/jquery.validate.js',
+  routes.app + routes.js +'libs/bootstrap.min.js',
+  routes.app + routes.js +'libs/jquery.bxslider.min.js',
+  routes.app + routes.js +'libs/jquery.magnific-popup.min.js',
+  routes.app + routes.js +'libs/velocity.min.js'
+];
 
 
 //Tarea para comprimir las libreriras JS
-gulp.task('libs', function() {  
+gulp.task('libs',  () =>{  
      return gulp.src(jsLibs)
         .pipe(header(banner, { pkg : pkg } ))
         .pipe(concat('concat.libs.js'))
-        .pipe(gulp.dest('publication/js'))
+        .pipe(gulp.dest(routes.app + routes.js))
         .pipe(rename('libs.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('publication/js'));
+        .pipe(gulp.dest(routes.app + routes.js));
 });
 
-
 //tarea para compilar stylus
-gulp.task('css', function () {
-  return gulp.src('src/stylus/main.styl')
+gulp.task('css',  () =>{
+  return gulp.src(routes.src + routes.stylus + 'main.styl')
     .pipe(header(banner, { pkg : pkg } ))
     .pipe(sourcemaps.init()) //cargamos tarea de sourcemaps
   .pipe(stylus({ //iniciamos stylus
@@ -65,9 +101,9 @@ gulp.task('css', function () {
   })) 
   .on('error', onError)
   .pipe(rename('style.css')) //renombramos el archivo
-  .pipe(gulp.dest('./publication/css')) // destino del archivo
-  .pipe(sourcemaps.write('.')) //creamos sourcemap aparte
-  .pipe(gulp.dest('./publication/css'))
+  .pipe(gulp.dest(routes.app + routes.css)) // destino del archivo
+  .pipe(sourcemaps.write('../maps')) //creamos sourcemap aparte
+  .pipe(gulp.dest(routes.app + routes.css))
   .pipe(browserSync.reload({
       stream: true
     }))
@@ -75,18 +111,21 @@ gulp.task('css', function () {
 });
 
 //Concatenar y minificar CSS
-gulp.task('minicss', function() {
-  return gulp.src(['publication/css/**/*.css', '!publication/css/**/'+pkg.name+'.min.css'])
-  .pipe(minifyCSS())
+gulp.task('minicss',  () =>{
+  return gulp.src([routes.app + routes.css + '**/*.css', '!'+routes.app + routes.css +'/**/'+pkg.name+'.min.css'])
   .pipe(concat(pkg.name +'.min.css'))
-  .pipe(gulp.dest('publication/css'))
+  // .pipe(uncss({
+  //            html: ['publication/**/*.html'] //Borramos css que no se usa
+  //  }))
+  .pipe(minifyCSS())
+  .pipe(gulp.dest(routes.app + routes.css))
 
 });
 
 
 //Render de pug
-gulp.task('views', function() {
-  return gulp.src('src/views/*.pug')
+gulp.task('views',  () =>{
+  return gulp.src(routes.src + routes.views + '*.pug')
     .pipe(data( function (file) {
       return {
         debug: debug,
@@ -97,7 +136,7 @@ gulp.task('views', function() {
     pretty: true
     }))
   .on('error', onError)
-  .pipe(gulp.dest('./publication'))
+  .pipe(gulp.dest(routes.app))
   .pipe(browserSync.reload({
       stream: true
     }))
@@ -106,7 +145,7 @@ gulp.task('views', function() {
 
 
 //Tarea base de browsersync para crear el servidor
-gulp.task('browserSync', function() {
+gulp.task('browserSync',  () =>{
   browserSync.init({
     server: {
       baseDir: 'publication/'
@@ -114,18 +153,19 @@ gulp.task('browserSync', function() {
   })
 });
 
-gulp.task('limpiar', function (done) {
+gulp.task('limpiar', (done) =>{
   return cache.clearAll(done);
 });
 
 
 //tarea que observa cambios para recargar el navegador
-gulp.task('watch', ['browserSync', 'views', 'css'], function (){
+gulp.task('watch', ['browserSync', 'views', 'css'],  () =>{
 
-  gulp.watch('src/stylus/**/*.styl',  ['css']);
-  gulp.watch(['src/views/*.pug', 'src/templates/**/*.pug'],  ['views']);
+  gulp.watch( routes.src + routes.stylus +'**/*.styl',  ['css']); //Stylus
+  gulp.watch([routes.src + routes.views + '*.pug', routes.src + routes.templates + '**/*.pug'],  ['views']); //Pug
   gulp.watch('publication/js/**/*.js', browserSync.reload);
-  gulp.watch('publication/images/**/*.{gif,svg,jpg,png}', browserSync.reload);
-  gulp.watch('publication/fonts/**/*.{svg,eot,ttf,woff,woff2}', browserSync.reload);
+  gulp.watch(routes.app + 'images/**/*.{gif,svg,jpg,png}', browserSync.reload); //Images
+  gulp.watch(routes.app + 'fonts/**/*.{svg,eot,ttf,woff,woff2}', browserSync.reload); //Fonts
 
 });
+
