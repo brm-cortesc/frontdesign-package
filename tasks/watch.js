7e83cdd36cd18bedbd645e42986ee48d5faec5f9
Cristian Cortés <cristian.cortes@brm.com.co>
2017-12-06 15:25:02 -0500

    updated tasks

--- tasks/watch.js
+++ tasks/watch.js
@@ -9,35 +9,42 @@ const data = require('./../frontend.json');
 const imagesPath = data.app + data.assets + 'images/**/*.{gif,svg,jpg,png}';
 const fontPath = data.app + data.assets + 'fonts/**/*.{svg,eot,ttf,woff,woff2}';
 const libsPath = data.app + data.assets + 'libs/**/*';
+const cssPath = data.app + data.assets + data.css;
 
 
-gulp.task('watch', ['browserSync', 'assets:img', 'assets:fonts', 'assets:js'], ()=>{
+
+gulp.task('watch', ['browserSync', 'assets:img', 'assets:fonts', 'assets:js', 'assets:css'], ()=>{
 
   watch(data.app + data.stylus, ()=>{
-  	gulp.start('css', 'csslint')
+    gulp.start('css', 'csslint')
   } );
 
 
   watch( [data.app + data.views, data.app + data.templates], ()=>{
-  	gulp.start('views')
+    gulp.start('views')
    });
 
   watch(data.app + data.babel, ()=>{
-  	gulp.start('js');
+    gulp.start('js');
   });
 
   watch(imagesPath, ()=>{
-  	gulp.start('assets:img');
+    gulp.start('assets:img');
 
   });
 
   watch(fontPath, ()=>{
-  	gulp.start('assets:fonts');
+    gulp.start('assets:fonts');
 
   });
 
   watch(libsPath, ()=>{
     gulp.start('assets:js');
+
+  });
+
+   watch(libsPath, ()=>{
+    gulp.start('assets:css');
 
   });
 
