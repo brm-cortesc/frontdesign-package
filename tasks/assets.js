2688ce22bd546f2a4c4027169e07635be98aee25
Cristian Cortés <cristian.cortes@brm.com.co>
2017-12-06 15:27:08 -0500

    bug fixed

--- tasks/assets.js
+++ tasks/assets.js
@@ -5,26 +5,34 @@ const gulp        = require('gulp'),
 const data = require('./../frontend.json');
 
 const imagesPath = data.app + data.assets + 'images/**/*.{gif,svg,jpg,png}',
-	  fontPath = data.app + data.assets + 'fonts/**/*.{svg,eot,ttf,woff,woff2}',
-	  libsPath = data.app + data.assets + data.js + 'libs/**/**';
+    fontPath = data.app + data.assets + 'fonts/**/*.{svg,eot,ttf,woff,woff2}',
+    libsPath = data.app + data.assets + data.js + 'libs/**/**';
+    cssPath = data.app + data.assets + data.css;
 
 
 gulp.task('assets:img', ()=>{
-	return gulp.src(imagesPath)
-		   .pipe(gulp.dest(data.build + data.assets + data.images ))
+  return gulp.src(imagesPath)
+       .pipe(gulp.dest(data.build + data.assets + data.images ))
 
 });
 
 
 gulp.task('assets:fonts', ()=>{
-	return gulp.src(fontPath)
-		   .pipe(gulp.dest(data.build + data.assets + data.fonts ))
+  return gulp.src(fontPath)
+       .pipe(gulp.dest(data.build + data.assets + data.fonts ))
 
 });
 
 
 gulp.task('assets:js', ()=>{
-	return gulp.src(libsPath)
-		   .pipe(gulp.dest(data.build + data.assets + data.js + 'libs/' ))
+  return gulp.src(libsPath)
+       .pipe(gulp.dest(data.build + data.assets + data.js + 'libs/' ))
+
+});
+
+
+gulp.task('assets:css', ()=>{
+  return gulp.src(libsPath)
+       .pipe(gulp.dest(data.build + data.assets + data.css ))
 
 });
--- tasks/css.js
+++ tasks/css.js
@@ -1,38 +1,89 @@
-const gulp        = require('gulp'),
-      path        = require('path');
+const gulp       = require('gulp'),
+      reload     = require('browser-sync').reload,
+      concat     = require('gulp-concat'), 
+      rename     = require('gulp-rename'),
+      minifyCSS  = require('gulp-minify-css'),
+      header     = require('gulp-header'),
+      notify     = require('gulp-notify'),
+      sourcemaps = require('gulp-sourcemaps'),
+      stylus     = require('gulp-stylus'),
+      nib        = require('nib'),
+      stylint    = require('gulp-stylint'),
+      path       = require('path'),      
+      plumber    = require('gulp-plumber');
+
 
 //datos
 const data = require('./../frontend.json');
 
-const imagesPath = data.app + data.assets + 'images/**/*.{gif,svg,jpg,png}',
-    fontPath = data.app + data.assets + 'fonts/**/*.{svg,eot,ttf,woff,woff2}',
-    libsPath = data.app + data.assets + data.js + 'libs/**/**';
-    cssPath = data.app + data.assets + data.css;
+//Stamps
+const banner = ['/**',
+  ' * <%= pkg.name %> - <%= pkg.description %>',
+  ' * @version v<%= pkg.version %>',
+  ' * @link <%= pkg.author.name %> - <%= pkg.author.email %>',
+  ' * @license <%= pkg.license %>',
+  ' *<%= new Date() %>',
+  ' */',
+  ''
+].join('\n');
 
 
-gulp.task('assets:img', ()=>{
-  return gulp.src(imagesPath)
-       .pipe(gulp.dest(data.build + data.assets + data.images ))
+//tarea para compilar stylus
+gulp.task('css',  () =>{
+  return gulp.src(data.app + data.stylus + 'main.styl')
+  .pipe(header(banner, { pkg : data } ))
+  .pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}))
+  .pipe(sourcemaps.init()) //cargamos tarea de sourcemaps
+  .pipe(stylus({ //iniciamos stylus
+    use: nib(), // cargamos nib para uso de css3
+    compress: false
+  }))
+  .pipe(rename('style.css')) //renombramos el archivo
+  .pipe(gulp.dest(data.build + data.assets + data.css)) // destino del archivo
+  .pipe(sourcemaps.write('../../maps')) //creamos sourcemap aparte
+  .pipe(gulp.dest(data.build + data.assets + data.css))
+  .pipe(reload({
+      stream: true
+    }))
 
 });
 
 
-gulp.task('assets:fonts', ()=>{
-  return gulp.src(fontPath)
-       .pipe(gulp.dest(data.build + data.assets + data.fonts ))
+//tarea para ver errores en sintaxis & semantica de stylus
+gulp.task('csslint', () =>{
+  return gulp.src([data.app + data.stylus + '**/*.styl', '!'+data.app + data.stylus + 'bootstrap/**/*.**',  '!'+data.app + data.stylus + 'font-awesome-stylus/**/*.**'  ])
+        .pipe(stylint({
+          rules:{
+            'sortOrder': 'alphabetical',
+            'namingConvention': {
+                'expect': 'lowercase-dash',
+                'error': true
+            },
+            'leadingZero': false,
+            'commentSpace': false,
+            'valid':{
+              'expect':true,
+              'error':true
+            },
+            'groupOutputByFile': true,
+            'namingConventionStrict': true,
+            'prefixVarsWithDollar': 'always'
+
+          }
+
+        }))
+        //.pipe(plumber())notify('<%= options.message %>')
+        .pipe(stylint.reporter({
+          logger: notify.onError()
+        }))
 
 });
 
-
-gulp.task('assets:js', ()=>{
-  return gulp.src(libsPath)
-       .pipe(gulp.dest(data.build + data.assets + data.js + 'libs/' ))
-
-});
-
-
-gulp.task('assets:css', ()=>{
-  return gulp.src(libsPath)
-       .pipe(gulp.dest(data.build + data.assets + data.css ))
+//Concatenar y minificar CSS
+gulp.task('minicss',  () =>{
+  return gulp.src([data.build + data.assets + data.css + '**/*.css', '!'+data.app + data.css +'/**/'+data.name+'.min.css'])
+  .pipe(concat(data.name +'.min.css'))
+  .pipe(minifyCSS())
+  .pipe(gulp.dest(data.build + data.assets + data.css))
 
 });
